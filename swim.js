f40a4c4f3d3928b039140420bec93647d081e1cc
Max Harrison <152919274+Max20001Harrison@users.noreply.github.com>
2023-12-04 22:00:55 +0200

    Create swim.js

---
+++ swim.js
@@ -0,0 +1,28 @@
+import '@testing-library/jest-dom/extend-expect'
+import { setConfig } from 'next/config'
+import config from './next.config'
+
+
+ "coins": [
+      {
+        "denom": "uatom",
+        "displayName": "ATOM",
+        "fraction": 1000000
+      },
+      {
+        "denom": "ubedrock",
+        "displayName": "UBEDROCK",
+        "fraction": 1000000
+      },
+      {
+// i'd like to eat
+
+
+        "denom": "uusd",
+        "displayName": "UST",
+        "fraction": 1000000
+      },
+      {
+        "denom": "urun",
+        "displayName": "RUN",
+        "fraction": 1000000
